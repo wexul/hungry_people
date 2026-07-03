5f34fe470c29d40eaed87d59fc3d8433c1275402
wexul <clevereryn@gmail.com>
2026-07-03 21:45:21 +0300

    specialties

---
+++ static/main/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.specialties__slider').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false
+    });
+});
